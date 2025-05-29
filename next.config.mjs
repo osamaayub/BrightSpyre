ef5a08ba25754543c9539bb4b62833540d4a3800
Osama <osama.ayubwebdev.com>
2025-05-29 14:23:25 +0500

    fixed signup route in clerkMiddleware

--- next.config.mjs
+++ next.config.mjs
@@ -15,8 +15,6 @@ const nextConfig = {
       },
     ],
     unoptimized: false,
-  },
-    allowedDevOrigins: ['http://13.213.30.63:3000'], // Add any origin accessing your dev server
-}
+  }}
 
 export default nextConfig
